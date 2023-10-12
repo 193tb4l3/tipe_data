8011e0195478914cc6bd7cdfacc7465d2a1700cf
anak desa 1503 <147463999+193tb4l3@users.noreply.github.com>
2023-10-12 08:01:48 +0800

    Add files via upload

---
+++ tipe_ data.py
@@ -0,0 +1,12 @@
+nama = "Upin" #str (teks)
+nim = "220602052" #str
+prodi = "teknik informatika" #str
+thn_masuk= 2022 #int (bil. bulat)
+ipk = 3.70 #float (bil. desimal)
+lunas_spp = True #boolean (benar/salah)
+
+print(f"{nama} dengan {nim} adalah mahasiswa {prodi} angkatan {thn_masuk}")
+
+print (lunas_spp) 
+
+
